ed3b5f2801709052e64f94a60a17da9ca87e67be
ugochukwujason <ugohjasonbobo@gmail.com>
2024-10-13 10:11:12 +0100

    commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//CALCULATOR PROGRAM
+
+const display = document.getElementById("display")
+
+function appendToDisplay(input){
+display.value +=input;
+}
+
+function cleardisplay(){
+     display.value="";
+}
+
+function calculate(){
+    display.value = eval(display.value);
+}
